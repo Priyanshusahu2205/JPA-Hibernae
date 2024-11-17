9e0f4cbc3d97e7c8d954e75c541a86cc99771849
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-11-17 06:13:57 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,60 @@
+package com.demo;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String username;
+	
+	@OneToMany(mappedBy="user")
+	private List<Task> tasks;
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(long id, String username, List<Task> tasks) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.tasks = tasks;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+	
+	
+
+}
