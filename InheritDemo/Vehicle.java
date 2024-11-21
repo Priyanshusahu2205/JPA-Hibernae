a44859cca81c3ef4190a73306b23f4dc8e7050a6
Priyanshu Sahu <priyanshusahu2205@gmail.com>
2024-11-21 05:34:26 +0530

    Add files via upload

---
+++ InheritDemo/Vehicle.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Vehicle {
+	
+	@Id
+	@GeneratedValue
+	private long id;
+
+	public Vehicle() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Vehicle(long id) {
+		super();
+		this.id = id;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
